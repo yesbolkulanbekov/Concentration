9359c3f94aed019d6d5da60f7052537dd1f10cc4
Yesbol Kulanbekov <ykulanbekov@nu.edu.kz>
2017-11-19 16:39:13 +0600

    Lecture 1 end

---
+++ Concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Yesbol Kulanbekov on 11/19/17.
+//  Copyright © 2017 Yesbol Kulanbekov. All rights reserved.
+//
+
+import Foundation
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Yesbol Kulanbekov on 11/19/17.
+//  Copyright © 2017 Yesbol Kulanbekov. All rights reserved.
+//
+
+import Foundation
